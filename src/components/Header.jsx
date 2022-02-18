d8d27bb48e08a1b286a8e985b0977f816f4818a1
Rounak Singh <rounaksingh9431738764@gmail.com>
2022-02-18 20:17:07 +0530

    Removed Used import

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,6 @@
 import React from "react";
 import SearchIcon from "@material-ui/icons/Search";
 import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
-import { Link } from "react-router-dom";
 
 // Files Import
 import { useStateValue } from "../Context/StateProvider";
